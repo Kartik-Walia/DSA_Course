56a55904c1e6c17da8e01847ef80c7bbdd6faaec
Kartik-Walia <itzkartikeds3@gmail.com>
2023-12-04 21:48:18 +0530

    Kruskal's Algorithm (Shortest Path in DAG remaining)

--- 12 Graphs/15_Kruskal_MST.cpp
+++ 12 Graphs/15_Kruskal_MST.cpp
@@ -1,11 +1,17 @@
 #include<iostream>
+#include<algorithm>     // For sort function 
 using namespace std;
+
+// Writing your own comparator for sorting according to weight 
+bool cmp(vector<int>& a, vector<int>& b) {
+    return a[2] < b[2];
+}
 
 // Initializing parent & rank data structures
 void makeSet(vector<int> &parent, vector<int> &rank, int N) {
     for(int i=0; i<N; i++) {
-        parent[i] = rank[i];
-        rank[1] = 0;
+        parent[i] = i;
+        rank[i] = 0;
     }
 }
 
@@ -22,7 +28,7 @@ int findParent(vector<int> &parent, int node) {
 }
 
 // Union Set 
-void unionSet(int u, int v, vector<int> &parent) {
+void unionSet(int u, int v, vector<int> &parent, vector<int> &rank) {
     // Firstly find parents of both 
     u = findParent(parent, u);
     v = findParent(parent, v);
@@ -39,12 +45,93 @@ void unionSet(int u, int v, vector<int> &parent) {
 }
 
 int MST(vector<vector<int> > &edges, int N) {
+    // Sort the edges on basis of weights
+    sort(edges.begin(), edges.end(), cmp);
+    // Sorting function time complexity is mlogm, where m is total number of  edges 
+
+    // Initializing parent & rank data structures
     vector<int> parent(N);
     vector<int> rank(N);
     makeSet(parent, rank, N);
+
+    int minWeight = 0;
+    for(int i=0; i<edges.size(); i++) {
+        int u = findParent(parent, edges[i][0]);
+        int v = findParent(parent, edges[i][1]);
+        int wt = edges[i][2];
+
+        if(u != v) {        // When parents are different, merege/union
+            minWeight += wt;
+            unionSet(u, v, parent, rank);
+        }
+    }
+
+    return minWeight;
 }
 
 int main(){
-    
+    int N, E; // Number of vertices and edges
+    cout << "Enter the number of vertices: ";
+    cin >> N;
+    cout << "Enter the number of edges: ";
+    cin >> E;
+
+    vector<vector<int> > edges;
+
+    cout << "Enter details for each edge (source, destination, weight):" << endl;
+    for (int i = 0; i < E; i++) {
+        int source, destination, weight;
+        cout << "Edge " << i + 1 << ": ";
+        cin >> source >> destination >> weight;
+        vector<int> temp;
+        temp.push_back(source);
+        temp.push_back(destination);
+        temp.push_back(weight);
+        edges.push_back(temp);
+    }
+
+    int minSpanningTreeWeight = MST(edges, N);
+
+    cout << "Minimum Spanning Tree Weight: " << minSpanningTreeWeight << endl;
+
     return 0;
 }
+
+/*
+
+Sample Input 1
+4
+4
+0 1 3
+0 3 5
+1 2 1
+2 3 8
+Sample Output 1
+Minimum Spanning Tree Weight: 9
+
+Sample Input 2
+4
+4
+1 2 6
+2 3 2
+1 3 2
+1 0 2
+Sample Output 2
+Minimum Spanning Tree Weight: 6
+
+Sample Input 3
+6
+9
+1 2 2
+1 4 1
+1 5 4
+4 5 9
+4 3 5
+2 4 3
+2 3 3
+2 6 7
+3 6 8
+Sample Output 3
+Minimum Spanning Tree Weight: 17
+
+*/
