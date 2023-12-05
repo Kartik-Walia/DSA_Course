6c98554d304a86678dcc5a39edddb82ce2da6d93
Kartik-Walia <itzkartikeds3@gmail.com>
2023-12-05 14:58:01 +0530

    Q2b Nov2022 EndSem

---
+++ Questions/PYQs/EndSems/CO_Nov2019/2B.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+void partitionEvenOdd(int A[], int n) {
+    int left = 0;
+    int right = n - 1;
+
+    while (left < right) {
+        // Move left pointer to the right until an odd number is found
+        while (A[left] % 2 == 0 && left < right) {
+            left++;
+        }
+
+        // Move right pointer to the left until an even number is found
+        while (A[right] % 2 != 0 && left < right) {
+            right--;
+        }
+
+        // Swap the even and odd elements
+        swap(A[left], A[right]);
+    }
+}
+
+// Sample usage
+int main() {
+    int A[] = {2, 7, 5, 10, 8, 3, 6};
+    int n = sizeof(A) / sizeof(A[0]);
+
+    cout << "Original Array: ";
+    for (int i = 0; i < n; i++) {
+        cout << A[i] << " ";
+    }
+    cout << endl;
+
+    partitionEvenOdd(A, n);
+
+    cout << "Partitioned Array: ";
+    for (int i = 0; i < n; i++) {
+        cout << A[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
