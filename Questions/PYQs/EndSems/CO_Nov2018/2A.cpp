a998b43fa20c5ea9470e5931b7852ad0fd4a1b55
Kartik-Walia <itzkartikeds3@gmail.com>
2023-12-05 13:10:57 +0530

    Q2a Nov2018 CO ENDSEM

---
+++ Questions/PYQs/EndSems/CO_Nov2018/2A.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <algorithm> // For sort function
+using namespace std;
+
+// Writing your own comparator for sorting according to weight
+bool cmp(vector<int> &a, vector<int> &b)
+{
+    return a[2] < b[2];
+}
+
+// Initializing parent & rank data structures
+void makeSet(vector<int> &parent, vector<int> &rank, int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        parent[i] = i;
+        rank[i] = 0;
+    }
+}
+
+// Finding parent
+int findParent(vector<int> &parent, int node)
+{
+    // Base case
+    if (parent[node] == node)
+    { // Means you've reached the final parent in that Hierarchy (like 4 in case of 7 -> 6 -> 4)
+        return node;
+    }
+
+    return findParent(parent, parent[node]);
+    // For Path Compression, replace above return statement with below return statement
+    // return parent[node] = findParent(parent, parent[node]);
+}
+
+// Union Set
+void unionSet(int u, int v, vector<int> &parent, vector<int> &rank)
+{
+    // Firstly find parents of both
+    u = findParent(parent, u);
+    v = findParent(parent, v);
+
+    // Now compare Ranks of parents
+    if (rank[u] < rank[v])
+    {
+        parent[u] = v;
+    }
+    else if (rank[v] < rank[u])
+    {
+        parent[v] = u;
+    }
+    else
+    {
+        parent[v] = u;
+        rank[u]++;
+    }
+}
+
+int MST(vector<vector<int>> &edges, int N, int &connectComp)
+{
+    // Sort the edges on basis of weights
+    sort(edges.begin(), edges.end(), cmp);
+    // Sorting function time complexity is mlogm, where m is total number of  edges
+
+    // Initializing parent & rank data structures
+    vector<int> parent(N);
+    vector<int> rank(N);
+    makeSet(parent, rank, N);
+
+    int minWeight = 0;
+    connectComp = N;
+    for (int i = 0; i < edges.size(); i++)
+    {
+        int u = findParent(parent, edges[i][0]);
+        int v = findParent(parent, edges[i][1]);
+        int wt = edges[i][2];
+
+        if (u != v)
+        { // When parents are different, merege/union
+            minWeight += wt;
+            unionSet(u, v, parent, rank);
+            connectComp--;
+        }
+    }
+
+    return minWeight;
+}
+
+int main()
+{
+    int N, E; // Number of vertices and edges
+    cout << "Enter the number of vertices: ";
+    cin >> N;
+    cout << "Enter the number of edges: ";
+    cin >> E;
+
+    vector<vector<int>> edges;
+
+    cout << "Enter details for each edge (source, destination, weight):" << endl;
+    for (int i = 0; i < E; i++)
+    {
+        int source, destination, weight;
+        cout << "Edge " << i + 1 << ": ";
+        cin >> source >> destination >> weight;
+        vector<int> temp;
+        temp.push_back(source);
+        temp.push_back(destination);
+        temp.push_back(weight);
+        edges.push_back(temp);
+    }
+
+    int connectComp;
+    int minSpanningTreeWeight = MST(edges, N, connectComp);
+
+    cout << "Minimum Spanning Tree Weight: " << minSpanningTreeWeight << endl;
+    cout << "Number of connected components: " << connectComp << endl;
+
+    return 0;
+}
+
+/*
+
+Connected Components Sample Input
+5
+4
+0 1 1
+0 2 1
+1 2 1
+3 4 1
+Minimum Spanning Tree Weight: 3
+Number of connected components: 2
+
+*/
